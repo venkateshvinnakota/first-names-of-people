958268c8ebd5a91aaf52d32076824237890b495e
venkatesh <venkey.vinnakota@gmail.com>
2022-11-23 06:15:15 +0000

    first names of people

---
+++ names/index.js
@@ -0,0 +1,9 @@
+const names = require("../country/state/city/index");
+
+const getFirstNames = require("../utilities/utils/index");
+
+const getPeopleInCity = (names) => {
+  return getFirstNames(names);
+};
+
+module.exports = getPeopleInCity;
